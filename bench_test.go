1a96f48de6ecd190ba7db7aa949cc2cb85191edd
Oleg Lobanov <oleg@lobanov.me>
2017-05-18 01:05:41 +0300

    fix benchmarks

--- bench_test.go
+++ bench_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math/rand"
 	"sort"
+	"sync"
 	"testing"
 )
 
@@ -10,6 +11,7 @@ func BenchmarkA(b *testing.B) {
 	a1 := createSlice(1000)
 	a2 := createSlice(5000)
 	a3 := createSlice(10000)
+	b.ResetTimer()
 	arr := [][]int{a1, a2, a3}
 	numbersSet := make(map[int]struct{})
 	for i := 0; i < b.N; i++ {
@@ -30,9 +32,21 @@ func BenchmarkB(b *testing.B) {
 	a1 := createSlice(1000)
 	a2 := createSlice(5000)
 	a3 := createSlice(10000)
+	b.ResetTimer()
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go sortInts(a1, &wg)
+	go sortInts(a2, &wg)
+	go sortInts(a3, &wg)
+	wg.Wait()
 	for i := 0; i < b.N; i++ {
 		MergeAndDedup(a1, a2, a3)
 	}
+}
+
+func sortInts(arr []int, wg *sync.WaitGroup) {
+	sort.Ints(arr)
+	wg.Done()
 }
 
 func createSlice(l int) []int {
